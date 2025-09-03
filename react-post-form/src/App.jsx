e9dcc85126dd45a0b1654c6d89518e4808a15e02
Steopa2001 <moraru495@gmail.com>
2025-09-03 14:58:59 +0200

    feat: create a single one state for all forms

--- react-post-form/src/App.jsx
+++ react-post-form/src/App.jsx
@@ -1,10 +1,16 @@
 //importo useState
 import { useState } from "react";
 //importo axios
-import axios from 'axios';
+import axios from "axios";
 
 function App() {
-
+  // stato unico che contiene tutti i campi del form
+  const [formData, setFormData] = useState({
+    author: "",
+    title: "",
+    body: "",
+    public: false,
+  });
 }
 
-export default App
+export default App;
