fcfe21c886f6bb5b6479474f30189eec07a58f2f
Steopa2001 <moraru495@gmail.com>
2025-09-03 15:59:32 +0200

    feat: create a state for the loading and alert

--- react-post-form/src/App.jsx
+++ react-post-form/src/App.jsx
@@ -4,6 +4,10 @@ import { useState } from "react";
 import axios from "axios";
 
 function App() {
+
+  const apiUrl = 'https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts';
+  console.log(apiUrl)
+
   // stato unico che contiene tutti i campi del form
   const [formData, setFormData] = useState({
     author: "",
@@ -11,6 +15,13 @@ function App() {
     body: "",
     public: false,
   });
+
+  // Stato di caricamento (true mentre la chiamata è in corso)
+  const [loading, setLoading] = useState(false);
+
+  // Stato per i messaggi di feedback (success / error)
+  const [alert, setAlert] = useState(null);
+
 }
 
 export default App;
